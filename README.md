5dfd6b20c7006bc7fbd39c9baecc1641760af765
Han-16 <haanbk16@gmail.com>
2023-03-17 10:01:32 +0900

    기본적인 lottery smart contract 구현

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lottery Smartcontract!
+
+클론코딩을 통한 lottery smart contract 연습
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,14 +5,45 @@ contract Lottery {
   
   address public owner;
   address payable [] public players;
-
+  uint256 public lotteryId;
+  mapping(uint256 => address) public lotteryHistory;
 
   constructor() {
     owner = msg.sender;
   }
 
+  modifier OnlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
   function enter() public payable {
     require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ETH");
+    players.push(payable(msg.sender));
+  }
+
+  function getBalance() public view returns (uint) {
+    return address(this).balance;
+  }
+
+  function getPlayers() public view returns (address payable[] memory) {
+    return players;
+  }
+
+  function getRandomNumber() public view returns (uint) {
+    return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+  }
+
+  function pickWinner() public OnlyOwner {
+    uint index = getRandomNumber() % players.length;
+
+    lotteryHistory[lotteryId] = players[index];
+    lotteryId++;
+
+    (bool success, ) = players[index].call{ value: address(this).balance }("");
+    require(success, "Failed to send ETH");
+
+    players = new address payable[](0);
   }
 
 }
